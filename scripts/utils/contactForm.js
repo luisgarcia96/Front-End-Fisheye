65d4204889dc37f8b924abee3f33dc4fc835c1d5
Luis Garcia <luisarmandogarcia96@gmail.com>
2023-02-09 20:41:46 +0100

    Create contact modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -7,3 +7,11 @@ function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
+function onSubmit(event) {
+    event.preventDefault();
+    console.log('Prénom: ', event.target[0].value);
+    console.log('Nom: ', event.target[1].value);
+    console.log('Email: ', event.target[2].value);
+    console.log('Message: ', event.target[3].value);
+}

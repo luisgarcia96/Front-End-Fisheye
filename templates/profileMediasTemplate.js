8becf7226a24213d5eae7d500052c63b84d1ea6a
Luis Garcia <luisarmandogarcia96@gmail.com>
2023-02-14 21:49:11 +0100

    Add filter selector

--- templates/profileMediasTemplate.js
+++ templates/profileMediasTemplate.js
@@ -30,12 +30,22 @@ export function getProfileMediasTemplate(photographer, photographerMedia) {
         <section class="photographer-medias">
             <div class="filter-bar-container">
                 <p class="filter-message">Trier par: </p>
-                <div class="filter-modal">
-                    <ul class="filters">
-                    <li class="filter">Popularité</li>
-                    <li class="filter">Date</li>
-                    <li class="filter">Titre</li>
-                    </ul>
+                <div class="filter-container">
+                    <div class="filters filter-selected">
+                        <div class="filter filter-1"></div>
+                        <div class="chevron-up">
+                            <i class="fa-solid fa-chevron-up"></i>
+                        </div>
+                        <div class="chevron-down">
+                            <i class="fa-solid fa-chevron-down"></i>
+                        </div>
+                    </div>
+                    <div class="filters filters-not-selected">
+                        <hr>
+                        <div class="filter filter-2"></div>
+                        <hr>
+                        <div class="filter filter-3"></div>
+                    </div>
                 </div>
             </div>
             ${mediasContainer.outerHTML}
